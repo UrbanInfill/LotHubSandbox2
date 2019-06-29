693949e02194c2f3a9a9879316a72681a356ffeb
Muhammad Noufal <Noufal21@users.noreply.github.com>
2019-06-29 11:10:26 +0500

    Update propertyEmail.blade.php

--- resources/views/propertyEmail.blade.php
+++ resources/views/propertyEmail.blade.php
@@ -8,20 +8,18 @@
                 <div class="col">
                     <div class="card" >
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <h5 class="card-title">Template 1</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
+                            <a href=" {{Request::url() }}" class="card-link">Card link</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <h5 class="card-title">Template 2</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="card-link">Card link</a>
+                            <a href="{{Request::url() }}/1" class="card-link">Card link</a>
                         </div>
                     </div>
 
