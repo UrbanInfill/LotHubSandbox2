d2e0771ee024a7403100c735d25f065a70dbe464
Muhammad Noufal <Noufal21@users.noreply.github.com>
2019-06-29 11:22:37 +0500

    Update propertyEmail.blade.php

--- resources/views/propertyEmail.blade.php
+++ resources/views/propertyEmail.blade.php
@@ -10,7 +10,7 @@
                         <div class="card-body">
                             <h5 class="card-title">Template 1</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href=" /propertymail//{{$fullname}}/{{$fulladdress}}" class="card-link">Use this template</a>
+                            <a href=" /propertymail/{{$fullname}}/{{$fulladdress}}" class="card-link">Use this template</a>
                         </div>
                     </div>
                 </div>
@@ -19,7 +19,7 @@
                         <div class="card-body">
                             <h5 class="card-title">Template 2</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="/propertymail//{{$fullname}}/{{$fulladdress}}/1" class="card-link">Use this template</a>
+                            <a href="/propertymail/{{$fullname}}/{{$fulladdress}}/1" class="card-link">Use this template</a>
                         </div>
                     </div>
 
