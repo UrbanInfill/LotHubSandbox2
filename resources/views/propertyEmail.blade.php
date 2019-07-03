6702a672feb746e71bdaf70d01815f4ce4bdf302
Noufal <noufalsiddiqui@gmail.com>
2019-07-03 16:55:06 +0500

    Update propertyEmail.blade.php

--- resources/views/propertyEmail.blade.php
+++ resources/views/propertyEmail.blade.php
@@ -50,9 +50,7 @@
         <div class="card-footer">
 
             <div class="input-group mb-3" style="width: 500px; float: right;">
-                <div contenteditable="true">
-                    {{$emailaddress}}
-                </div>
+                
                 <input type="email" id="propertyEmailTxt" value="{{$emailaddress}}" class="form-control" placeholder="Enter Email address" aria-label="Email address" aria-describedby="button-addon2">
                 <div class="input-group-append">
                     <button  class="btn btn-secondary" id="SendMail" style="float: right; ">Send email</button>
