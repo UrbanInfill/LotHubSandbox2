460732c98582a0122e0e3452d201569415c077a4
Work <noufalsiddiqui@gmail.com>
2019-07-07 20:15:43 +0500

    add id 1

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-fixed-top navbar-dark">
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
