7016c7965b6c066bf3d18d9e173dede8716dd924
Work <noufalsiddiqui@gmail.com>
2019-07-12 11:22:18 +0500

    Update personDetail.blade.php

--- resources/views/personDetail.blade.php
+++ resources/views/personDetail.blade.php
@@ -1,32 +1,60 @@
 @extends('master')
 @section('title','Saved Properties')
 @section('content')
-<div class="card">
-    <div class="card-header">
-        <h4>Detail information</h4>
-    </div>
-    <div class="card-body">
-        <div class="card">
-            <div class="card-header">
-                Names list
-            </div>
-            <div class="card-body">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>
-                                Full Name
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody id="NameTableBody">
+    <div class="card">
+        <div class="card-header">
+            <h4>Detail information</h4>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            Names list
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>
+                                        Full Name
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody id="NameTableBody">
 
-                    </tbody>
-                </table>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            DOB list
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>
+                                        Date
+                                    </th>
+                                    <th>
+                                        Age
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody id="dobTableBody">
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 
@@ -44,7 +72,12 @@
         const nameList = personDetail.name.map((names)=>{
             return "<tr>"+"<td>"+names.data+"</td>"+"</th>";
         });
+        const dobList = personDetail.dob.map((item)=>{
+            return "<tr>"+"<td>"+item.date+"</td>"+"<td>"+item.age+"</td">+"</th>";
+        });
+
         $('#NameTableBody').append(nameList);
+        $('#dobTableBody').append(dobList);
         console.log( personDetail );
 
     </script>
