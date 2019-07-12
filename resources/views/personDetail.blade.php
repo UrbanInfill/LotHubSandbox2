3f7cc7fb9bfc9f1bb9a9b76c15f1feb0bfa7d17e
Work <noufalsiddiqui@gmail.com>
2019-07-12 12:00:06 +0500

    Update personDetail.blade.php

--- resources/views/personDetail.blade.php
+++ resources/views/personDetail.blade.php
@@ -7,6 +7,7 @@
         </div>
         <div class="card-body">
             <div class="row">
+                <!-- First Col  -->
                 <div class="col">
                     <div class="row">
                         <div class="col">
@@ -54,7 +55,46 @@
                             </div>
                         </div>
                     </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-header">
+                                    RelationShip list
+                                </div>
+                                <div class="card-body">
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <td>
+                                                First Name
+                                            </td>
+                                            <td>
+                                                Last Name
+                                            </td>
+                                            <td>
+                                                Age
+                                            </td>
+                                            <td>
+                                                Date of Birth
+                                            </td>
+                                            <td>
+                                                Type
+                                            </td>
+                                            <td>
+                                                Phone Number Lists
+                                            </td>
+                                        </tr>
+                                        </thead>
+                                        <tbody id="RelationTableBody">
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <!-- Second Column -->
                 <div class="col">
                     <div class="row">
                         <div class="col">
@@ -145,11 +185,17 @@
         const EmailList = personDetail.email.map((item)=>{
             return "<tr>"+"<td>"+item.data+"</td>"+"</th>";
         });
+        const relationList = personDetail.relationship.map((item)=>{
+            return "<tr>"+"<td>"+item.name.first+"</td>"+"<td>"+item.name.last+"</td>"+"<td>"+item.dob.age+"</td>"+"<td>"+item.dob.date.data+"</td>"+item.dob.type+"</td>"+ "<td> <ul>"+
+                item.phone.map((num)=>"<li>"+num.number+"</li>") +
+                "<ul> </td>"+ "</th>";
+        });
 
         $('#NameTableBody').append(nameList);
         $('#dobTableBody').append(dobList);
         $('#PhoneTableBody').append(phoneList);
         $('#EmailTableBody').append(EmailList);
+        $('#RelationTableBody').append(relationList);
         console.log( personDetail );
 
     </script>
