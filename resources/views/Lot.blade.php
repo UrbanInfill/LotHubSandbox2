a815f5478773bb3b285dec4bea930a3b97cb457a
Work <noufalsiddiqui@gmail.com>
2019-07-12 23:55:11 +0500

    Update Lot.blade.php

--- resources/views/Lot.blade.php
+++ resources/views/Lot.blade.php
@@ -8,7 +8,7 @@
                      <h4><small> <strong>You have {{$Rcout}} Historic Lot Searches Left. Search Limit will refresh in {{$timeExceed}} </strong></small></h4>
                     <br>
                     <div class="embed-responsive embed-responsive-4by3">
-                        <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=dqcdh-W53Ag&feature=youtu.be" ></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/dqcdh-W53Ag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <label class="form-text text-muted" for="searchByPropForm">Enter the Zip-Code Below to Find Historically Platted Lots</label>
                     <div class="input-group  mb-3 search search-reduce" id="searchByPropForm">
