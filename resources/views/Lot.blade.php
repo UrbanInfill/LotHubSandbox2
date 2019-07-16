af9d5f484b81ab4ee0784ef731657756da4b7940
Work <noufalsiddiqui@gmail.com>
2019-07-16 19:09:38 +0500

    Update Lot.blade.php

--- resources/views/Lot.blade.php
+++ resources/views/Lot.blade.php
@@ -5,18 +5,21 @@
         <div class="row">
             <div class="col">
                 <div style="display: flex">
-
-                    <div style="justify-content: center; width: 560px">
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <iframe  src="https://www.youtube.com/embed/dqcdh-W53Ag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="row">
+                        <div class='col' style="justify-content: center; width: 560px" >
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe  src="https://www.youtube.com/embed/dqcdh-W53Ag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
                         </div>
                     </div>
-                    <div style="justify-content: center">
+                    <div class="row">
+                        <div class="col" style="justify-content: center">
 
-                        <h4><small> <strong>You have {{$Rcout}} Historic Lot Searches Left. Search Limit will refresh in {{$timeExceed}} </strong></small></h4>
+                            <h4><small> <strong>You have {{$Rcout}} Historic Lot Searches Left. Search Limit will refresh in {{$timeExceed}} </strong></small></h4>
 
-                        <br>
-                        <label class="form-text text-muted" for="searchByPropForm">Enter the Zip-Code Below to Find Historically Platted Lots</label>
+                            <br>
+                            <label class="form-text text-muted" for="searchByPropForm">Enter the Zip-Code Below to Find Historically Platted Lots</label>
+                        </div>
                     </div>
                 </div>
                 <div class="form-group">
