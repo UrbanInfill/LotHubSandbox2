afdb5ba065a35966b596fe936ff185e5636098c1
Work <noufalsiddiqui@gmail.com>
2019-07-16 19:14:54 +0500

    Update Lot.blade.php

--- resources/views/Lot.blade.php
+++ resources/views/Lot.blade.php
@@ -7,16 +7,17 @@
                 <div style="display: flex">
                     <div class="row">
                         <div class='col' style="justify-content: center; width: 560px" >
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col" style="justify-content: center">
                             <h3>Learn How to Find Deals with This Lothub Tutorial Video </h3>
                             <div class="embed-responsive embed-responsive-16by9">
                                 <iframe  src="https://www.youtube.com/embed/dqcdh-W53Ag" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
 
                             <h3>Ready to Find Hot Property Deals Nationwide? Enter the Desired Zip-Code Below</h3>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col" style="justify-content: center">
 
                             <h4><small> <strong>You have {{$Rcout}} Historic Lot Searches Left. Search Limit will refresh in {{$timeExceed}} </strong></small></h4>
 
