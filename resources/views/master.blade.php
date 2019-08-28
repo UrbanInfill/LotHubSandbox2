0373d44e17ffef35e1e4e53b822a627d32b7c075
Work <noufalsiddiqui@gmail.com>
2019-08-28 16:20:17 +0200

    g

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -99,6 +99,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js" integrity="sha384-7aThvCh9TypR7fIc2HV4O/nFMVCBwyIUKL8XCtKE+8xgCgl/PQGuFsvShjr74PBp" crossorigin="anonymous"></script>
 <script src="{{ url('/js/bootstrap-notify.min.js') }}"></script>
+<script src="{{ url('/js/cookies.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 <script type="module"  src=" {{ url('/js/siema.js') }}"></script>
 <script src=" {{ url('/js/main.js') }}"></script>
