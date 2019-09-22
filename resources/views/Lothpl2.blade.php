937525a228225757b9cd52b3f5c77d169085b228
UrbanInfill <48097838+UrbanInfill@users.noreply.github.com>
2019-09-22 11:34:26 -0700

    Update Lothpl2.blade.php

--- resources/views/Lothpl2.blade.php
+++ resources/views/Lothpl2.blade.php
@@ -1,7 +1,19 @@
 @extends('master')
 @section('title','Historically Platted Lots')
 @section('content')
-    <div class="pt-3">
+   
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""/>
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+ <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+   integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+   crossorigin=""></script>
+   
+   
+   
+   
+   <div class="pt-3">
         <div class="row">
             <div class="col" style="justify-content: center">
                 <img src="{{URL::asset('/Img/Lot_Hub_Banner.png')}}" alt="" class="mx-auto d-block">
@@ -41,6 +53,7 @@
 
             <div class="map" id="map"></div>
 
+ <div id="mapid"></div>
 
 
             <div class="row">
